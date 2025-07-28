80666d6bf029f57a07b15632192d61083eccc882
Aviral Singh Chauhan <blr.aviral@gmail.com>
2025-07-29 00:16:56 +0530

    fix: updates about section para

--- src/JS/app.js
+++ src/JS/app.js
@@ -1,82 +1,11 @@
 /*
     ? Aviral Singh Chauhan, 10/2022
 */
+
 document.getElementById("root").onclick = function (e) {
   hideMenu();
   hideContextMenu();
 };
-
-const contact = `
-  <div class="contact-info">
-    <h4>You can reach me on: </h4>
-    <ul class="contact-list">
-      <li>Email: <a href="mailto:blr.aviral@gmail.com">blr.aviral@gmail.com</a></li>
-      <li>Social: <a href="https://www.linkedin.com/in/aviral-s-79955a119/" target="_blank">LinkedIn</a>, <a href="https://github.com/sAVItar02">Github</a></li>
-    </ul>
-  </div>
-
-  <br/>
-  <hr>
-  <br/>
-`;
-
-const about = `
-  <div class="about-container">
-    <div class="about-img flex-center">
-      <img src="./../../public/assets/about-img.jfif" alt="About Image"/>
-    </div>
-    <div class="about-focus">
-      <p class="fw-bold">Hi, I'm Aviral 😄</p>
-      <p>I'm a 22-year-old Web Developer living in <a href="https://en.wikipedia.org/wiki/Bangalore">Bengaluru</a></p>
-      <p>I enjoy building pages that live on the web, my interest in web development started back in 2019 when i took a <a href="https://www.udemy.com/course/the-web-developer-bootcamp/" target="_blank">Web Development Course</a> out of curiosity and ended up learning a lot about the web.</p>
-      <p>Today, after having the opportunity to intern and learn at  <a href="https://www.linkedin.com/company/ultrainstinct-ai/" target="_blank">UltraInstinct</a>, <a href="https://www.linkedin.com/company/ascendance-st/mycompany/" target="_blank">Ascendance</a> and <a href="https://www.eurofins.in/eitsi/" target="_blank">Eurofins</a>, My primary focus is working on Intelligent Automation, chatbots and AI at <a href="https://www.linkedin.com/company/pwc-india/">PwC</a> </p>
-    </div>
-    <hr>
-    <div class="about-tech">
-      <p class="fw-bold">Few technologies I've been working with recently: </p>
-      <div class="about-tech-list-buffer">
-        <ul class="about-tech-list-1">
-          <li>JavaScript (ES6+)</li>
-          <li>Solidity</li>
-          <li>ReactJS</li>
-        </ul>
-        <ul class="about-tech-list-2">
-          <li>Web3</li>
-          <li>TypeScript</li>
-          <li>Node.js</li>
-        </ul>
-      </div>
-    </div>
-    <hr class="hr-block">
-    <div class="about-footer">
-      <p>This site is created from the ground up utilising a unique framework that was lovingly created <a href="https://github.com/sAVItar02/portfolio-js" target="_blank">entirely in JavaScript</a>.</p>
-    </div>
-  </div>
-`;
-
-const credits = `
-  <div class="banner-buffer">
-    <div class="banner-img"></div>
-  </div>
-  <div class="credits-container">
-    <ul class="list">
-      <li class="list-item">Main Background (Landscape Gif) : <a target="_blank" href="https://in.pinterest.com/pin/667166132311196309/">@anasabdin</a></li>
-      <li class="list-item">Japanese City Background Gif : <a target="_blank" href="https://in.pinterest.com/pin/726768458635723246/">@Retronator</a></li>
-      <li class="list-item">Night City Background Gif : <a target="_blank" href="https://in.pinterest.com/pin/726768458635317793/">@Aliciel</a></li>
-      <li class="list-item">Factory Background Gif : <a target="_blank" href="https://in.pinterest.com/pin/726768458635723269/">@A Cidade Branca</a></li>
-      <li class="list-item">Hourglass Loader Gif : <a target="_blank" href="https://giphy.com/stickers/transparent-aYKTYtCYb2ECSKfyal">@paperpixelco</a></li>
-      <li class="list-item">About Image : <a target="_blank" href="https://in.pinterest.com/pin/366621225903655441/">@leeoccleshaw</a></li>
-      <li class="list-item">Github Icon : <a target="_blank" href="http://pixelartmaker.com/art/d7e4e1e509c728d">Pixel Art Maker</a></li>
-      <li class="list-item">Settings Icon : <a target="_blank" href="http://pixelartmaker.com/art/ac6c0486a959d41">Pixel Art Maker</a></li>
-      <li class="list-item">Cookie Image : <a target="_blank" href="http://pixelartmaker.com/art/8d93c7834a63bf9">Pixel Art Maker</a></li>
-      <li class="list-item">Font : <a target="_blank" href="https://github.com/blobject/agave">Agave</a></li>
-      <li class="list-item">Battery Icon : <a target="_blank" href="https://fontawesome.com/search?q=battery&o=r">Fontawesome</a></li>
-      <li class="list-item">Design Credits : <a target="_blank" href="https://jewelcodes.io/">@jewelcodes</a></li>
-    </ul>
-  </div>
-`;
-
-const noticeText = `<img class="notice-img" src="./../../public/assets/cookie.png" alt="cookie image"/> <div>This website uses cookies to deliver the best possible experience, by changing any settings you consent to using cookies.</div>`;
 
 function openUrl(url) {
   window.open(url);
